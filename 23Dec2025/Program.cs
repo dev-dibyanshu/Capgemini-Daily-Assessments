39aeba7f4ca2363fd31a9260ab8149703ea56479
Dev Dibyanshu <devdibyanshu12321@gmail.com>
2025-12-24 09:34:34 +0530

    Added 23 Dec files

---
+++ 23Dec2025/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student defaultStudent = new Student();
+        
+        Console.WriteLine("Default Student:");
+        defaultStudent.DisplayInfo();
+        
+        Console.Write("Enter student name: ");
+        string name = Console.ReadLine();
+        
+        Console.Write("Enter student age: ");
+        int age = int.Parse(Console.ReadLine());
+        
+        Console.Write("Enter student grade: ");
+        string grade = Console.ReadLine();
+        
+        Student newStudent = new Student(name, age, grade);
+        
+        Console.WriteLine("New Student:");
+        newStudent.DisplayInfo();
+    }
+}
